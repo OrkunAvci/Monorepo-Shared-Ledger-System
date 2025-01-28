98b0fd2bdacab8be462b472d2c6353a0a6175daa
Orkun AVCI <orkunavci28@gmail.com>
2025-01-28 14:25:05 +0300

    Fix typo

--- freelance/src/api/ledgers/schemas.py
+++ freelance/src/api/ledgers/schemas.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 from core.ledgers.schemas import BaseLedgerOperation
 
-class FreelancingLedgerOperation(BaseLedgerOperation):
+class FreelancingLedgerOperation(BaseLedgerOperation, Enum):
     PROJECT_PAYMENT = "PROJECT_PAYMENT"  # Credit: Incoming payment for a project
     PROJECT_SPENDING = "PROJECT_SPENDING"  # Debit: Outgoing spending for a project
