33365bcbf908cf2f2bed87fd03798eff6ac501fd
Orkun AVCI <orkunavci28@gmail.com>
2025-01-28 15:33:24 +0300

    Fix typo

--- core/ledgers/schemas.py
+++ core/ledgers/schemas.py
@@ -1,6 +1,6 @@
-import enum
+from enum import Enum
 
-class BaseLedgerOperation(enum.Enum):
+class BaseLedgerOperation(Enum):
     CREDIT = "CREDIT"        # Add credits
     DEBIT = "DEBIT"          # Subtract credits
     TRANSFER = "TRANSFER"    # Move credits between accounts
--- freelance/src/api/ledgers/schemas.py
+++ freelance/src/api/ledgers/schemas.py
@@ -3,5 +3,5 @@ from enum import Enum
 from core.ledgers.schemas import BaseLedgerOperation
 
 class FreelancingLedgerOperation(BaseLedgerOperation, Enum):
-    PROJECT_PAYMENT = "PROJECT_PAYMENT"  # Credit: Incoming payment for a project
-    PROJECT_SPENDING = "PROJECT_SPENDING"  # Debit: Outgoing spending for a project
+    PROJECT_PAYMENT = "PROJECT_PAYMENT"  # Incoming payment for a project
+    PROJECT_SPENDING = "PROJECT_SPENDING"  # Outgoing spending for a project
